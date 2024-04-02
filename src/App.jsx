c6576ad8f9df7da5a2b9f7e144b99f36b45be0ad
JP <juanpabloduette@gmail.com>
2024-04-02 20:25:50 -0300

    tailwind instaled

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,12 @@ function App() {
   
   return (
     <>
-      <h1>Sublimatek</h1>
+     
+      <div className="h-52 w-52 bg-sky-400 flex justify-center items-center">
+      <h1 className="text-slate-700">Sublimatek</h1>
       <BsHandbag/>
+      </div>
+      
     </>
   )
 }
