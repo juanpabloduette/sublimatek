1406b3544f887c2bb02d8d4181010d7e5b73ced3
JP <juanpabloduette@gmail.com>
2024-04-02 21:27:39 -0300

    intentando usar zustand

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { BsHandbag } from "react-icons/bs";
+import useStore from "./store/counter";
+
 
 function App() {
-  
+//  const counter = useStore(state => state.count)
+//  const increment = useStore(state => state.inc)
+
+ const { count, inc } = useStore()
   return (
     <>
-     
+
       <div className="h-52 w-52 bg-sky-400 flex justify-center items-center">
-      <h1 className="text-slate-700">Sublimatek</h1>
+      <h1 className="text-slate-700">Sublimatek {count}</h1>
       <BsHandbag/>
+      <button onClick={inc}>+1</button>
       </div>
       
     </>
