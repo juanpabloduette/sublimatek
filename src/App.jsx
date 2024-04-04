7bf1c7f9aaad99936efa68fa1f676ecd05349420
Kevin Contreras <contreraskevin200@gmail.com>
2024-04-04 15:12:55 -0300

    pruebas counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
-import { BsHandbag } from "react-icons/bs";
-import useStore from "./store/counter";
-
+import { BsHandbag } from 'react-icons/bs';
+import useStore from './store/counter';
 
 function App() {
-//  const counter = useStore(state => state.count)
-//  const increment = useStore(state => state.inc)
+	//  const counter = useStore(state => state.count)
+	//  const increment = useStore(state => state.inc)
 
- const { count, inc } = useStore()
-  return (
-    <>
-
-      <div className="h-52 w-52 bg-sky-400 flex justify-center items-center">
-      <h1 className="text-slate-700">Sublimatek {count}</h1>
-      <BsHandbag/>
-      <button onClick={inc}>+1</button>
-      </div>
-      
-    </>
-  )
+	const { count, inc, dec, reset } = useStore();
+	return (
+		<>
+			<div className='h-52 w-52 bg-sky-400 flex justify-center items-center'>
+				<h1 className='text-slate-700'>Sublimatek {count}</h1>
+				<BsHandbag />
+			</div>
+			<button onClick={inc} className='bg-black text-slate-50 p-5'>
+				+1
+			</button>
+			<button onClick={reset} className='bg-red-600 text-slate-50 p-5'>
+				reset
+			</button>
+			<button onClick={dec} className='bg-slate-700 text-slate-50 p-5'>
+				-1
+			</button>
+		</>
+	);
 }
 
-export default App
+export default App;
--- src/store/counter.js
+++ src/store/counter.js
@@ -1,8 +1,11 @@
-import { create } from 'zustand'
+import { create } from 'zustand';
 
-const useStore = create((set) => ({
-  count: 1,
-  inc: () => set((state) => ({ count: state.count + 1 })),
-}))
+const useStore = create(set => ({
+	initialValue: 0,
+	count: 1,
+	inc: () => set(state => ({ count: state.count + 1 })),
+	dec: () => set(state => (state.count > 0 ? { count: state.count - 1 } : {})),
+	reset: () => set(state => ({ count: state.initialValue })),
+}));
 
-export default useStore
+export default useStore;
