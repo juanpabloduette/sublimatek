da387f262e98936cb23f85dcb7002b3be22db7df
JP <juanpabloduette@gmail.com>
2024-04-08 03:14:33 -0300

    building dropdown de navbar

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -14,20 +14,22 @@ const Header = () => {
         <div className="text-stone-50"><img src={logo} alt="Sublimatek"/></div>
         <nav className={`nav_items ${isOpen && "open"}`}>
                 <Link to='/' onClick={ () => setIsOpen(!isOpen)}>Inicio</Link>
-                <Link to='/' onClick={ () => setIsOpen(!isOpen)}>Categorias<IoIosArrowDown /></Link>
+                <Link to='/' onClick={ () => setIsOpen(!isOpen)}>Categorias</Link><IoIosArrowDown  color="white"/>
                 <Link to='/' onClick={ () => setIsOpen(!isOpen)}>Como comprar</Link>
                 <Link to='/' onClick={ () => setIsOpen(!isOpen)}>Mayoristas</Link>
                 <Link to='/Contacto' onClick={ () => setIsOpen(!isOpen)}>Contacto</Link>
         </nav>
-        <div className={`nav_toggle ${isOpen && "open"}`} onClick={ () => setIsOpen(!isOpen)} >
-                <span></span>
-                <span></span>
-                <span></span>
-        </div>
-        <div className="cart relative m-2">
-            <BsHandbag  color="white" size={35} />
-            <div className="w-5 h-5 bg-red-600 rounded-full absolute top-5 left-4">
-                <p className="text-xs absolute left-1.5 top-0.5 text-stone-50">{count}</p>
+        <div className="flex items-center">
+            <div className="cart relative m-2">
+                <BsHandbag  color="white" size={32} />
+                <div className="w-5 h-5 bg-red-600 rounded-full absolute top-4 left-4">
+                    <p className="text-xs absolute left-1.5 top-0.5 text-stone-50">{count}</p>
+                </div>
+            </div>
+            <div className={`nav_toggle ${isOpen && "open"}`} onClick={ () => setIsOpen(!isOpen)} >
+                    <span></span>
+                    <span></span>
+                    <span></span>
             </div>
         </div>
     </header>
---
+++ src/components/layout/header/Menuitems.js
@@ -0,0 +1,27 @@
+export const categoriesDropdown = [
+    {
+      id: 1,
+      title: "Colección Clubber",
+      path: "./marketing",
+      cName: "submenu-item",
+    },
+    {
+      id: 2,
+      title: "Consulting",
+      path: "./consulting",
+      cName: "submenu-item",
+    },
+    {
+      id: 3,
+      title: "Design",
+      path: "./design",
+      cName: "submenu-item",
+    },
+    {
+      id: 4,
+      title: "Development",
+      path: "./development",
+      cName: "submenu-item",
+    },
+  ];
+  
