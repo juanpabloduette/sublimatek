3b2e224ff603e9931024d1959142c0ca5de3b86f
JP <juanpabloduette@gmail.com>
2024-04-11 16:32:15 -0300

    Cambios en develop finales

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -5,23 +5,44 @@ import "./Header.css"
 import logo from '../../../assets/images/logo.png';
 import useStore from '../../../store/counter';
 import { IoIosArrowDown } from "react-icons/io";
+import { Menu,MenuItem, Button, Typography} from '@mui/material';
 
 const Header = () => {
     const [isOpen, setIsOpen] = useState(false)
     const { count } = useStore();
+    const [open, setOpen] = useState(null)
+	const handleClose = () => {
+		setOpen(null)
+	}
+	const handleClick = (e) => {
+		setOpen(e.currentTarget)
+	}
+
     return (
     <header className="flex justify-between items-center bg-black">
         <div className="text-stone-50"><img src={logo} alt="Sublimatek"/></div>
         <nav className={`nav_items ${isOpen && "open"}`}>
                 <Link to='/' onClick={ () => setIsOpen(!isOpen)}>Inicio</Link>
-                <div className="flex items-center">
-                    <div>
-                        <Link to='/Categories' onClick={ () => setIsOpen(!isOpen)}>Categorias</Link>
-                    </div>
-                    <div>
-                        <IoIosArrowDown  color="white"/> 
-                    </div>
-                </div>
+                <Link to='' onClick={ () => setIsOpen(!isOpen)}>
+                    <Button id='basic-button' size='small' style={{ padding: 0 }} endIcon={<IoIosArrowDown  color="white"/>} onClick={handleClick}>
+                        <Typography variant="body1" style={{ fontFamily: 'sistem-ui', fontSize: '11px' }}>Categorias</Typography>
+                    </Button>
+                    <Menu
+                    id="basic-menu"
+                    anchorEl={open}
+                    open={Boolean(open)}
+                    onClose={handleClose}
+                    autoFocus={false}
+                    transitionDuration={200}
+                    >
+                        <MenuItem onClick={handleClose}>Colección Clubber</MenuItem>
+                        <MenuItem onClick={handleClose}>Colección Gamer</MenuItem>
+                        <MenuItem onClick={handleClose}>Colección Anime</MenuItem>
+                        <MenuItem onClick={handleClose}>Colección Sport</MenuItem>
+                        <MenuItem onClick={handleClose}>Colección Music</MenuItem>
+                        <MenuItem onClick={handleClose}>Colección Series</MenuItem>
+                    </Menu> 
+                </Link>
                 <Link to='/' onClick={ () => setIsOpen(!isOpen)}>Como comprar</Link>
                 <Link to='/' onClick={ () => setIsOpen(!isOpen)}>Mayoristas</Link>
                 <Link to='/Contacto' onClick={ () => setIsOpen(!isOpen)}>Contacto</Link>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,12 @@
-import { Menu as MenuIcon } from '@mui/icons-material';
 import useStore from '../store/counter';
-import { useState } from 'react';
-import { Menu,MenuItem, Button} from '@mui/material';
-import { IoIosArrowDown } from "react-icons/io"; 
-
-import { Button } from '@mui/material';
 import Layout from '../components/layout/main/Layout';
 
 const Home = () => {
 	const { inc, dec, reset } = useStore();
-	
-	const [open, setOpen] = useState(null)
-	const handleClose = () => {
-		setOpen(null)
-	}
-	const handleClick = (e) => {
-		setOpen(e.currentTarget)
-	}
+		
 	return (
 		<>
-			<Layout>
+			<Layout headerVisible={true} footerVisible={true}>
 			<button onClick={inc} className='bg-black text-slate-50 p-5'>
 				+1
 			</button>
@@ -29,23 +16,6 @@ const Home = () => {
 			<button onClick={dec} className='bg-slate-700 text-slate-50 p-5'>
 				-1
 			</button>
-			<Button id='basic-button' size='small' endIcon={<IoIosArrowDown  color="white"/>} onClick={handleClick} onMouseOver={handleClick}>Categorias</Button>
-			
-			<Menu
-			id="basic-menu"
-			anchorEl={open}
-			open={Boolean(open)}
-			onClose={handleClose}
-			autoFocus={false}
-			transitionDuration={200}
-			>
-				<MenuItem onClick={handleClose}>Colección Clubber</MenuItem>
-				<MenuItem onClick={handleClose}>Colección Gamer</MenuItem>
-				<MenuItem onClick={handleClose}>Colección Anime</MenuItem>
-				<MenuItem onClick={handleClose}>Colección Sport</MenuItem>
-				<MenuItem onClick={handleClose}>Colección Music</MenuItem>
-				<MenuItem onClick={handleClose}>Colección Series</MenuItem>
-			</Menu>
 			</Layout>
 		</>
 	);
